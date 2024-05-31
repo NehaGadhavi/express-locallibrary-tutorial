b67c93eb7e591137af38dbf41cf6eadc35095a1e
NehaGadhavi <138009692+NehaGadhavi@users.noreply.github.com>
2024-05-31 17:36:53 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,82 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+const catalogRouter = require('./routes/catalog');
+const compression = require("compression");
+const helmet = require("helmet");
+
+var app = express();
+
+// Set up rate limiter: maximum of twenty requests per minute
+const RateLimit = require("express-rate-limit");
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
+// Apply rate limiter to all requests
+app.use(limiter);
+
+// Add helmet to the middleware chain.
+// Set CSP headers to allow our Bootstrap and Jquery to be served
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+    },
+  }),
+);
+
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
+const dev_db_url =
+  'mongodb+srv://nehagadhavi:zLIpz0UpVw65e37c@cluster0.8vp2edl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+main()
+  .then(() => {
+    console.log('connection established');
+  })
+  .catch((err) => console.log("error occured during connection establishment",err));
+
+async function main() {
+  return await mongoose.connect(mongoDB);
+}
+
+app.use(compression()); // Compress all routes
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/users/cool', usersRouter);
+app.use('/catalog', catalogRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
